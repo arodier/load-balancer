6fd11518c9ce8049ac55878016d9166d221debab
André Rodier <andre@rodier.me>
2015-10-08 17:42:27 +0100

    Handle Ctrl+C

--- src/main.go
+++ src/main.go
@@ -5,6 +5,8 @@ import (
     "fmt"
     "net/http"
     "os"
+    "os/signal"
+    "log"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -16,4 +18,15 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main() {
     http.HandleFunc("/", handler)
     http.ListenAndServe(":8484", nil)
+
+    // Properly handle Ctrl-C
+    channel := make(chan os.Signal, 1)
+    signal.Notify(channel, os.Interrupt)
+    go func() {
+        for _ = range channel {
+            log.Printf("Receiving interrupt. Bye...")
+            os.Exit(0)
+        }
+    }()
+
 }
